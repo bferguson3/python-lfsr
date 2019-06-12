b0b12faaefbbe08cbd10526891af4ec3ea93f489
bferguson3 <32964659+bferguson3@users.noreply.github.com>
2019-06-12 15:39:36 -0400

    Update lfsr.py

--- lfsr.py
+++ lfsr.py
@@ -2,7 +2,6 @@
 # Linear feedback shift register example
 
 # Example usage:
-#  import lfsr
 #  a = lfsr(random.randrange(0,255))
 #  print(format(int(a.tick_output(8),2), '02x'))
 # Will initialize a random 8-bit initeger in the register, 
@@ -15,7 +14,7 @@ from collections import deque
 
 class lfsr(object):
     ''' creates lfsr object '''
-    def __init__(self, init=26, taps=[3, 4, 5]):
+    def __init__(self, init=26, taps=[3, 4, 5, 7]):
         if init < 256:
             self.bin = format(init, '08b')
         elif init < 65536:
@@ -31,8 +30,24 @@ class lfsr(object):
 
     def shift(self):
         ''' performs shift ''' 
+        # rotate right
+        r = self.d.pop()
+        self.d.appendleft(r)
+        # first, check to see if there are any 1s in the tap positions
+        i = 0
+        ok = False
+        while i < len(self.d):
+            if self.d[i] == '1':
+                ok = True
+                break
+            i += 1
+        if ok == False:
+            self.shift()
+            return 
+
         t = self.d.pop()
         t_b = int(t,2)
+        self.output = t_b
         i = len(self.d)-1
         while i >= 0:
             for tap in self.taps:
@@ -40,7 +55,6 @@ class lfsr(object):
                     s = int(self.d[i],2)
                     t_b = s ^ t_b
             i -= 1
-        self.output = t_b
         self.d.appendleft(str(t_b))
         return self.d 
 
@@ -65,7 +79,7 @@ class lfsr(object):
         return '$' + format(int(out,2), '02x')
 
     def tick_output(self, n=1):
-        ''' returns 'n' outputs [0/1] concatenated to a single binary string '''
+        ''' returns 'n' outputs [0/1] concatenated to a single integer '''
         i = 0
         out = []
         while i < n:
